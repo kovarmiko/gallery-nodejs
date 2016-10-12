23a055a93521fdb9e94074a35dd091fb1aebcfdf
kovarmiko <kovarmiko@yahoo.com>
2016-10-12 16:42:39 +0200

    added models and create read part of the crud

---
+++ crudOps.js
@@ -0,0 +1,22 @@
+'use strict';
+var Picture = require('./models/Picture');
+module.exports = {
+	picture : {
+		create : (title, gallery, owner, url, uploaded, clb) => {
+			let picture = new Picture({
+				title,
+				gallery,
+				owner,
+				url,
+				uploaded
+			}).save(clb);
+			return picture;
+		},
+		all : (clb) => {
+			Picture.find(clb)
+		},
+		findById :(id, clb) => {
+			Picture.findOne( {'_id' : id }, clb);
+		}
+	}
+};
--- routes/api/pictures.js
+++ routes/api/pictures.js
@@ -1,11 +1,12 @@
-
+'use strict';
 var express = require('express');
 var router = express.Router();
+var crudOps = require('../../crudOps');
 
 var multer = require('multer');
 var storage =   multer.diskStorage({
   destination: function (req, file, callback) {
-    callback(null, './uploads');
+    callback(null, './public/uploads');
   },
   filename: function (req, file, callback) {
     callback(null, file.originalname);
@@ -14,25 +15,70 @@ var storage =   multer.diskStorage({
 var upload = multer({ storage: storage }).single('picture');
 
 /* Send Upload Form */
-/* GET home page. */
-router.get('/upload', function(req, res, next) {
-  res.render('uploader', {});
+router.get('/create', function(req, res, next) {
+  res.render('uploader', {message : "Picture Form"});
 });
 
 /* Process uploded Picture */
-router.post('/upload', function(req, res, next) {
-	console.log('loggin body');
-	console.dir(req.body); 
+router.post('/create', function(req, res, next) {
 
 	upload(req,res,function(err) {
         if(err) {
         	console.dir(err);
-            return res.end("Error uploading file.");
+            return res.render('uploader', {message : "Upload failed!"});
         }
-        res.end("File is uploaded");
+        //console.log(req.body);
+        //prepare values from the form
+        let title = req.body.Title;
+		let gallery = req.body.Gallery;
+		let owner  = req.body.Owner;
+        let url = req.file.filename;
+        let uploaded = Date.now();
+        
+        //create picture
+        let picture = crudOps.picture.create(title, gallery, owner, url, uploaded, (err, picture) => console.dir(picture));
+
+        
+        //send response of the server
+        res.render('uploader', {message : "Upload Successfull!"});
     });
 });
 
+router.get('/allPics', (req,res,next) => {
+	crudOps.picture.all((err, pictures) =>{
+		if(err){
+			console.log(err);
+			res.status(500);
+			res.send('server encountered an unexpected error');
+			return;	
+		}
+		res.render('gallery', {pictures});
+	});
+});
 
+router.get('/edit', (req,res,next) => {
+	let id = req.query.id;
+	
+	
+	crudOps.picture.findById(id, (err, picture) =>{
+		if(err){
+			console.log(err);
+			res.status(500);
+			res.send('server encountered an unexpected error');
+			return;	
+		}
+		//console.dir(picture);
+		res.render('edit-picture', {picture});
+	});
+});
+
+router.post('/edit', (req,res,next) => {
+//@todo: body of this form: 
+//this will edit metadata only
+//@todo: separate form handler for picture changes: 
+//this is because enctype="multipart/form-data" is not supported by bodyParser middleware
+	
+	res.send('posted');
+});
 
 module.exports = router;
