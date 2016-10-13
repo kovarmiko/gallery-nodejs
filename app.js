de7d5050894bb60296fa6e4295455db9c600e0b2
kovarmiko <kovarmiko@yahoo.com>
2016-10-13 10:39:23 +0200

    created login

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var login = require('./routes/login');
 var pictures = require('./routes/api/pictures');
 
 var app = express();
@@ -39,7 +40,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/pictures', pictures)
+app.use('/pictures', pictures);
+app.use('/login', login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
